e1dd4c7567c60a2ea41d05c89e15d2379fc28979
GreenLionXIII <92956129+GreenLionXIII@users.noreply.github.com>
2022-01-04 10:59:49 -0700

    Fixed Rush Sounds

---
+++ GoodRush/entry.lua
@@ -0,0 +1,166 @@
+-- Special thanks to RogueClaris (Hacker Owl) for helping me with moving Rush off the screen!
+
+
+nonce = function() end
+
+local BUSTER_SHOT_TEXTURE = Engine.load_texture(_modpath.."Buster_Shot.png")
+local MEGAMAN_TEXTURE = Engine.load_texture(_modpath.."Megaman.png")
+local BUSTER_SFX = Engine.load_audio(_modpath .. "Buster_Shot.ogg")
+local RUSH_SFX = Engine.load_audio(_modpath .. "Rush_Hit.ogg")
+
+local rush_alive = true
+
+function package_init(package) 
+    package:declare_package_id("com.Thor.card.GoodRush")
+    package:set_icon_texture(Engine.load_texture(_modpath.."icon.png"))
+    package:set_preview_texture(Engine.load_texture(_modpath.."preview.png"))
+	package:set_codes({'G', 'R'})
+
+    local props = package:get_card_props()
+    props.shortname = "GoodRush"
+    props.damage = 140
+    props.time_freeze = true
+    props.element = Element.None
+    props.description = "Sends Rush to attack"
+	props.card_class = CardClass.Mega
+	props.limit = 1
+end
+
+function card_create_action(actor, props)
+    print("in create_card_action()!")
+    local action = Battle.CardAction.new(actor, "PLAYER_IDLE")
+	action:set_lockout(make_sequence_lockout())
+	
+	
+	action.execute_func = function(self, user)
+		local actor = self:get_actor()
+		actor:hide()
+        print("in custom card action execute_func()!")
+		--user:hide()
+		actor:hide()
+		local step1 = Battle.Step.new()
+		local field = user:get_field()
+		local tile = user:get_tile(user:get_facing(), 0)
+		local mega = Battle.Artifact.new()
+		local do_once = true
+		local do_once_again = true
+		local self_variable = self
+		rush_alive = true
+		step1.update_func = function(self, dt)
+				if do_once then
+					do_once = false
+					mega:set_facing(user:get_facing())
+					mega:set_texture(MEGAMAN_TEXTURE, true)
+					mega:sprite():set_layer(-2)
+					
+					local mega_anim = mega:get_animation()
+					mega_anim:load(_modpath.."Megaman.animation")
+					mega_anim:set_state("Megaman_Spawn")
+					mega_anim:on_complete(function()
+						local cannonshot = create_rush(user, props)
+						local tile1 = user:get_tile(user:get_facing(), 1)
+						mega_anim:set_state("Megaman_Shoot")
+						self_variable:add_anim_action(2, function()
+							Engine.play_audio(BUSTER_SFX, AudioPriority.Highest)
+						end)
+						--Engine.play_audio(BUSTER_SFX, AudioPriority.Highest)
+						mega_anim:on_complete(function()
+							actor:get_field():spawn(cannonshot, tile1)
+						end)
+					end)
+					
+					actor:hide()
+					field:spawn(mega, tile)
+					
+				end
+				
+				if rush_alive == false then
+					if do_once_again then
+						do_once_again = false
+						local mega_anim = mega:get_animation()
+						mega_anim:set_state("Megaman_Despawn")
+						mega_anim:on_complete(function()
+							mega:erase()
+							actor:reveal()
+							--user:reveal()
+							self:complete_step()
+						end)
+					end
+				end
+				
+		end
+		self:add_step(step1)
+			
+	end
+	return action
+	
+end
+
+
+
+function create_rush(user, props)
+    local spell = Battle.Spell.new(user:get_team())
+    spell:set_texture(BUSTER_SHOT_TEXTURE, true)
+    --spell:highlight_tile(Highlight.Solid)
+	spell:set_height(16.0)
+	local direction = user:get_facing()
+    spell.slide_started = false
+	
+    spell:set_hit_props(
+        HitProps.new(
+            props.damage, 
+            Hit.Impact | Hit.Flinch, 
+            Element.None,
+            user:get_context(),
+            Drag.None
+        )
+    )
+	
+    local anim = spell:get_animation()
+    anim:load(_modpath.."Buster_Shot.animation")
+    anim:set_state("BUSTER_SHOT")
+    spell.update_func = function(self, dt) 
+		local offset = self:get_offset()
+        self:get_current_tile():attack_entities(self)
+        if self:is_sliding() == false then 
+			if spell:get_current_tile():is_edge() and self.slide_started then
+				if offset.x >= 150 then
+					self:delete()
+					rush_alive = false
+				else
+					self:set_offset(offset.x + 5, offset.y)
+				end
+			end
+            local dest = self:get_tile(direction, 1)
+            local ref = self
+            self:slide(dest, frames(15), frames(0), ActionOrder.Voluntary, 
+                function()
+                    ref.slide_started = true 
+                end
+            )
+        end
+    end
+
+
+	
+	
+	
+    spell.attack_func = function(self, other) 
+        -- nothing
+    end
+	
+	spell.collision_func = function(self, other)
+		Engine.play_audio(RUSH_SFX, AudioPriority.Highest)
+	end
+	
+    spell.delete_func = function(self) 
+		print("good boy rush")
+		self:erase()
+    end
+
+    spell.can_move_to_func = function(tile)
+        return true
+    end
+
+    return spell
+end
---
+++ LavaStge/entry.lua
@@ -0,0 +1,122 @@
+nonce = function() end
+local AUDIO1 = Engine.load_audio(_modpath.."PanelFinalChange.ogg")
+local AUDIO2 = Engine.load_audio(_modpath.."PanalChange.wav")
+
+function package_init(package) 
+	package:declare_package_id("com.Thor.card.LavaStge")
+	package:set_icon_texture(Engine.load_texture(_modpath.."icon.png"))
+	package:set_preview_texture(Engine.load_texture(_modpath.."preview.png"))
+	package:set_codes({'A', 'E', 'R', 'T', 'Y', '*'})
+
+	local props = package:get_card_props()
+	props.shortname = "LavaStge"
+	props.time_freeze = true
+	props.element = Element.Fire
+	props.description = "Changes all panls to lava"
+	props.limit = 5
+end
+
+function card_create_action(actor, props)
+    print("in create_card_action()!")
+	local action = Battle.CardAction.new(actor, "PLAYER_IDLE")
+	action:set_lockout(make_sequence_lockout())
+	action.execute_func = function(self, user)
+		local panel11 = user:get_field():tile_at( 1, 1 )
+		local panel12 = user:get_field():tile_at( 1, 2 )
+		local panel13 = user:get_field():tile_at( 1, 3 )
+		local panel21 = user:get_field():tile_at( 2, 1 )
+		local panel22 = user:get_field():tile_at( 2, 2 )
+		local panel23 = user:get_field():tile_at( 2, 3 )
+		local panel31 = user:get_field():tile_at( 3, 1 )
+		local panel32 = user:get_field():tile_at( 3, 2 )
+		local panel33 = user:get_field():tile_at( 3, 3 )
+		local panel41 = user:get_field():tile_at( 4, 1 )
+		local panel42 = user:get_field():tile_at( 4, 2 )
+		local panel43 = user:get_field():tile_at( 4, 3 )
+		local panel51 = user:get_field():tile_at( 5, 1 )
+		local panel52 = user:get_field():tile_at( 5, 2 )
+		local panel53 = user:get_field():tile_at( 5, 3 )
+		local panel61 = user:get_field():tile_at( 6, 1 )
+		local panel62 = user:get_field():tile_at( 6, 2 )
+		local panel63 = user:get_field():tile_at( 6, 3 )
+		
+		
+		local k = 0
+		local cooldown = 0
+		local step1 = Battle.Step.new()
+		step1.update_func = function(self, dt)
+			if cooldown <= 0 then
+				k = k + 1
+				cooldown = 0.05
+				Engine.play_audio(AUDIO2, AudioPriority.Low)
+				if user:get_tile():get_state() == TileState.Lava then
+					panel11:set_state(TileState.Normal)
+					panel12:set_state(TileState.Normal)
+					panel13:set_state(TileState.Normal)
+					panel21:set_state(TileState.Normal)
+					panel22:set_state(TileState.Normal)
+					panel23:set_state(TileState.Normal)
+					panel31:set_state(TileState.Normal)
+					panel32:set_state(TileState.Normal)
+					panel33:set_state(TileState.Normal)
+					panel41:set_state(TileState.Normal)
+					panel42:set_state(TileState.Normal)
+					panel43:set_state(TileState.Normal)
+					panel51:set_state(TileState.Normal)
+					panel52:set_state(TileState.Normal)
+					panel53:set_state(TileState.Normal)
+					panel61:set_state(TileState.Normal)
+					panel62:set_state(TileState.Normal)
+					panel63:set_state(TileState.Normal)
+				else
+					panel11:set_state(TileState.Lava)
+					panel12:set_state(TileState.Lava)
+					panel13:set_state(TileState.Lava)
+					panel21:set_state(TileState.Lava)
+					panel22:set_state(TileState.Lava)
+					panel23:set_state(TileState.Lava)
+					panel31:set_state(TileState.Lava)
+					panel32:set_state(TileState.Lava)
+					panel33:set_state(TileState.Lava)
+					panel41:set_state(TileState.Lava)
+					panel42:set_state(TileState.Lava)
+					panel43:set_state(TileState.Lava)
+					panel51:set_state(TileState.Lava)
+					panel52:set_state(TileState.Lava)
+					panel53:set_state(TileState.Lava)
+					panel61:set_state(TileState.Lava)
+					panel62:set_state(TileState.Lava)
+					panel63:set_state(TileState.Lava)
+				end
+			else
+				cooldown = cooldown - dt
+			end
+			
+			if k == 14 then
+				self:complete_step()
+			end	
+		end
+		self:add_step(step1)		
+		
+		panel11:set_state(TileState.Lava)
+		panel12:set_state(TileState.Lava)
+		panel13:set_state(TileState.Lava)
+		panel21:set_state(TileState.Lava)
+		panel22:set_state(TileState.Lava)
+		panel23:set_state(TileState.Lava)
+		panel31:set_state(TileState.Lava)
+		panel32:set_state(TileState.Lava)
+		panel33:set_state(TileState.Lava)
+		panel41:set_state(TileState.Lava)
+		panel42:set_state(TileState.Lava)
+		panel43:set_state(TileState.Lava)
+		panel51:set_state(TileState.Lava)
+		panel52:set_state(TileState.Lava)
+		panel53:set_state(TileState.Lava)
+		panel61:set_state(TileState.Lava)
+		panel62:set_state(TileState.Lava)
+		panel63:set_state(TileState.Lava)
+		Engine.play_audio(AUDIO1, AudioPriority.Low)
+	end
+	return action
+end
